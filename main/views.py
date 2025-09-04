ce2245c54d43c1bdfc5352b8b23faadbb2f56a9a
Prashantranamagar <ranamagar.prashant@gmail.com>
2025-09-04 15:51:57 +0545

    format code

--- main/views.py
+++ main/views.py
@@ -21,45 +21,71 @@ from blacklist.models import BlackList
 def home(request):
 
     site = Main.objects.get(pk=2)
-    news = News.objects.filter(act=1).order_by('-pk')
-    cat = Cat.objects.all()
+    news = News.objects.filter(act=1).order_by("-pk")
+    cat = Cat.objects.all()[:3]
     subcat = SubCat.objects.all()
-    lastnews = News.objects.filter(act=1).order_by('-pk')[:3]
-    popnews = News.objects.filter(act=1).order_by('-show')
-    popnews2 = News.objects.filter(act=1).order_by('-show')[:3]
-    trending = Trending.objects.all().order_by('-pk')[:5]
-    lastnews2 = News.objects.filter(act=1).order_by('-pk')[:4]
+    lastnews = News.objects.filter(act=1).order_by("-pk")[:3]
+    popnews = News.objects.filter(act=1).order_by("-show")
+    popnews2 = News.objects.filter(act=1).order_by("-show")[:3]
+    trending = Trending.objects.all().order_by("-pk")[:5]
+    lastnews2 = News.objects.filter(act=1).order_by("-pk")[:4]
 
-    return render(request, 'front/home.html', {'site':site, 'news':news, 'cat':cat, 'subcat':subcat, 'lastnews':lastnews, 'popnews':popnews, 'popnews2':popnews2, 'trending':trending, 'lastnews2':lastnews2})
-
+    return render(
+        request,
+        "front/home.html",
+        {
+            "site": site,
+            "news": news,
+            "cat": cat,
+            "subcat": subcat,
+            "lastnews": lastnews,
+            "popnews": popnews,
+            "popnews2": popnews2,
+            "trending": trending,
+            "lastnews2": lastnews2,
+        },
+    )
 
 
 def about(request):
-    
+
     site = Main.objects.get(pk=2)
-    news = News.objects.all().order_by('-pk')
+    news = News.objects.all().order_by("-pk")
     cat = Cat.objects.all()
     subcat = SubCat.objects.all()
-    lastnews = News.objects.all().order_by('-pk')[:3]
-    popnews2 = News.objects.all().order_by('-show')[:3]
-    trending = Trending.objects.all().order_by('-pk')[:5]
+    lastnews = News.objects.all().order_by("-pk")[:3]
+    popnews2 = News.objects.all().order_by("-show")[:3]
+    trending = Trending.objects.all().order_by("-pk")[:5]
 
-    return render(request, 'front/about.html', {'site':site, 'news':news, 'cat':cat, 'subcat':subcat, 'lastnews':lastnews, 'popnews2':popnews2, 'trending':trending})
+    return render(
+        request,
+        "front/about.html",
+        {
+            "site": site,
+            "news": news,
+            "cat": cat,
+            "subcat": subcat,
+            "lastnews": lastnews,
+            "popnews2": popnews2,
+            "trending": trending,
+        },
+    )
 
 
 def panel(request):
-    
+
     # login check start
-    if not request.user.is_authenticated :
-        return redirect('mylogin')
+    if not request.user.is_authenticated:
+        return redirect("mylogin")
     # login check end
 
     perm = 0
     perms = Permission.objects.filter(user=request.user)
-    for i in perms :
-        if i.codename == "master_user" : perm = 1
-    
-    '''
+    for i in perms:
+        if i.codename == "master_user":
+            perm = 1
+
+    """
     test = ['!' , '@' , '#' , '$' , '%']
     rand = ""
     for i in range(4):
@@ -70,159 +96,162 @@ def panel(request):
 
     count = News.objects.count()
     rand = News.objects.all()[random.randint(0,count-1)]
-    '''
+    """
 
-    
-
-
-    
-    return render(request, 'back/home.html', {})
+    return render(request, "back/home.html", {})
 
 
 def mylogin(request):
 
-    if request.method == 'POST' :
+    if request.method == "POST":
 
-        utxt = request.POST.get('username')
-        ptxt = request.POST.get('password')
+        utxt = request.POST.get("username")
+        ptxt = request.POST.get("password")
 
-        if utxt != "" and ptxt != "" :
+        if utxt != "" and ptxt != "":
 
             user = authenticate(username=utxt, password=ptxt)
 
-            if user != None :
+            if user != None:
 
                 ip, is_routable = get_client_ip(request)
 
-                if ip is None :
+                if ip is None:
 
                     ip = "0.0.0.0"
 
                 b = len(BlackList.objects.filter(ip=ip))
 
-                if b != 0 :
+                if b != 0:
                     msg = "Your ip Blocked By Admin"
-                    return render(request, 'front/msgbox.html', {'msg':msg})
+                    return render(request, "front/msgbox.html", {"msg": msg})
 
                 login(request, user)
-                return redirect('panel')
+                return redirect("panel")
 
-
-    return render(request, 'front/login.html')
+    return render(request, "front/login.html")
 
 
 def myregister(request):
-    
-    if request.method == 'POST':
 
-        name = request.POST.get('name')
-        uname = request.POST.get('uname')
-        email = request.POST.get('email')
-        password1 = request.POST.get('password1')
-        password2 = request.POST.get('password2')
+    if request.method == "POST":
 
-        if name == "" :
+        name = request.POST.get("name")
+        uname = request.POST.get("uname")
+        email = request.POST.get("email")
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+
+        if name == "":
             msg = "Input Your Name"
-            return render(request, 'front/msgbox.html', {'msg':msg})
+            return render(request, "front/msgbox.html", {"msg": msg})
 
-        
-        if password1 != password2 :
+        if password1 != password2:
             msg = "Your Pass Didn't Match"
-            return render(request, 'front/msgbox.html', {'msg':msg})
+            return render(request, "front/msgbox.html", {"msg": msg})
 
         count1 = 0
         count2 = 0
-        count3 = 0 
+        count3 = 0
         count4 = 0
 
-        for i in password1 :
+        for i in password1:
 
-            if i > "0" and i < "9" :
+            if i > "0" and i < "9":
                 count1 = 1
-            if i > "A" and i < "Z" :
+            if i > "A" and i < "Z":
                 count2 = 1
-            if i > 'a' and i < 'z' :
+            if i > "a" and i < "z":
                 count3 = 1
-            if i > "!" and i < "(" :
+            if i > "!" and i < "(":
                 count4 = 1
 
-        if count1 == 0 or count2 == 0 or count3 == 0 or count4 == 0 :
+        if count1 == 0 or count2 == 0 or count3 == 0 or count4 == 0:
             msg = "Your Pass Is Not Strong"
-            return render(request, 'front/msgbox.html', {'msg':msg})
+            return render(request, "front/msgbox.html", {"msg": msg})
 
-        if len(password1) < 8 :
+        if len(password1) < 8:
             msg = "Your Pass Most Be 8 Character"
-            return render(request, 'front/msgbox.html', {'msg':msg})
+            return render(request, "front/msgbox.html", {"msg": msg})
 
-        if len(User.objects.filter(username=uname)) == 0 and len(User.objects.filter(email=email)) == 0 :
+        if (
+            len(User.objects.filter(username=uname)) == 0
+            and len(User.objects.filter(email=email)) == 0
+        ):
 
             ip, is_routable = get_client_ip(request)
 
-            if ip is None :
+            if ip is None:
 
                 ip = "0.0.0.0"
 
-            try :
+            try:
 
-                response = DbIpCity.get(ip,api_key='free')
+                response = DbIpCity.get(ip, api_key="free")
                 country = response.country + " | " + response.city
 
             except:
 
                 country = "Unknown"
 
-            user = User.objects.create_user(username=uname,email=email,password=password1)
-            b = Manager(name=name,utxt=uname,email=email,ip=ip,country=country)
+            user = User.objects.create_user(
+                username=uname, email=email, password=password1
+            )
+            b = Manager(name=name, utxt=uname, email=email, ip=ip, country=country)
             b.save()
 
-    return render(request, 'front/login.html')
+    return render(request, "front/login.html")
 
 
 def mylogout(request):
 
     logout(request)
 
-    return redirect('mylogin')
+    return redirect("mylogin")
 
 
 def site_setting(request):
 
     # login check start
-    if not request.user.is_authenticated :
-        return redirect('mylogin')
+    if not request.user.is_authenticated:
+        return redirect("mylogin")
     # login check end
 
     perm = 0
     for i in request.user.groups.all():
-        if i.name == "masteruser" : perm = 1
+        if i.name == "masteruser":
+            perm = 1
 
-    if perm == 0 :
+    if perm == 0:
         error = "Access Denied"
-        return render(request, 'back/error.html' , {'error':error})
+        return render(request, "back/error.html", {"error": error})
 
+    if request.method == "POST":
 
-    if request.method == 'POST' :
+        name = request.POST.get("name")
+        tell = request.POST.get("tell")
+        fb = request.POST.get("fb")
+        tw = request.POST.get("tw")
+        yt = request.POST.get("yt")
+        link = request.POST.get("link")
+        txt = request.POST.get("txt")
 
-        name = request.POST.get('name')
-        tell = request.POST.get('tell')
-        fb = request.POST.get('fb')
-        tw = request.POST.get('tw')
-        yt = request.POST.get('yt')
-        link = request.POST.get('link')
-        txt = request.POST.get('txt')
+        if fb == "":
+            fb = "#"
+        if tw == "":
+            tw = "#"
+        if yt == "":
+            yt = "#"
+        if link == "":
+            link = "#"
 
-        if fb == "" : fb = "#"
-        if tw == "" : tw = "#"
-        if yt == "" : yt = "#"
-        if link == "" : link = "#"
-
-        if name == "" or tell == "" or txt == "" :
+        if name == "" or tell == "" or txt == "":
             error = "All Fields Requirded"
-            return render(request, 'back/error.html' , {'error':error})
+            return render(request, "back/error.html", {"error": error})
 
-        try : 
+        try:
 
-            myfile = request.FILES['myfile']
+            myfile = request.FILES["myfile"]
             fs = FileSystemStorage()
             filename = fs.save(myfile.name, myfile)
             url = fs.url(filename)
@@ -230,16 +259,14 @@ def site_setting(request):
             picurl = url
             picname = filename
 
-        except :
+        except:
 
             picurl = "-"
             picname = "-"
 
+        try:
 
-
-        try : 
-
-            myfile2 = request.FILES['myfile2']
+            myfile2 = request.FILES["myfile2"]
             fs2 = FileSystemStorage()
             filename2 = fs2.save(myfile2.name, myfile2)
             url2 = fs2.url(filename2)
@@ -247,12 +274,10 @@ def site_setting(request):
             picurl2 = url2
             picname2 = filename2
 
-        except :
+        except:
 
             picurl2 = "-"
             picname2 = "-"
-
-
 
         b = Main.objects.get(pk=2)
         b.name = name
@@ -262,46 +287,46 @@ def site_setting(request):
         b.yt = yt
         b.link = link
         b.about = txt
-        
-        if picurl != "-" : b.picurl = picurl
-        if picname != "-" : b.picname = picname
-        if picurl2 != "-" :  b.picurl2 = picurl2
-        if picname2 != "-" : b.picname2 = picname2
-        
+
+        if picurl != "-":
+            b.picurl = picurl
+        if picname != "-":
+            b.picname = picname
+        if picurl2 != "-":
+            b.picurl2 = picurl2
+        if picname2 != "-":
+            b.picname2 = picname2
+
         b.save()
-
-
-       
-
 
     site = Main.objects.get(pk=2)
 
-
-    return render(request, 'back/setting.html', {'site':site})
+    return render(request, "back/setting.html", {"site": site})
 
 
 def about_setting(request):
 
     # login check start
-    if not request.user.is_authenticated :
-        return redirect('mylogin')
+    if not request.user.is_authenticated:
+        return redirect("mylogin")
     # login check end
 
     perm = 0
     for i in request.user.groups.all():
-        if i.name == "masteruser" : perm = 1
+        if i.name == "masteruser":
+            perm = 1
 
-    if perm == 0 :
+    if perm == 0:
         error = "Access Denied"
-        return render(request, 'back/error.html' , {'error':error})
+        return render(request, "back/error.html", {"error": error})
 
-    if request.method == 'POST':
+    if request.method == "POST":
 
-        txt = request.POST.get('txt')
+        txt = request.POST.get("txt")
 
-        if txt == "" :
+        if txt == "":
             error = "All Fields Requirded"
-            return render(request, 'back/error.html' , {'error':error})
+            return render(request, "back/error.html", {"error": error})
 
         b = Main.objects.get(pk=2)
         b.abouttxt = txt
@@ -309,73 +334,84 @@ def about_setting(request):
 
     about = Main.objects.get(pk=2).abouttxt
 
-    return render(request, 'back/about_setting.html', {'about':about})
+    return render(request, "back/about_setting.html", {"about": about})
 
 
 def contact(request):
 
-
     site = Main.objects.get(pk=2)
-    news = News.objects.all().order_by('-pk')
+    news = News.objects.all().order_by("-pk")
     cat = Cat.objects.all()
     subcat = SubCat.objects.all()
-    lastnews = News.objects.all().order_by('-pk')[:3]
-    popnews2 = News.objects.all().order_by('-show')[:3]
-    trending = Trending.objects.all().order_by('-pk')[:5]
+    lastnews = News.objects.all().order_by("-pk")[:3]
+    popnews2 = News.objects.all().order_by("-show")[:3]
+    trending = Trending.objects.all().order_by("-pk")[:5]
 
-    return render(request, 'front/contact.html' , {'site':site, 'news':news, 'cat':cat, 'subcat':subcat, 'lastnews':lastnews, 'popnews2':popnews2, 'trending':trending})
-    
+    return render(
+        request,
+        "front/contact.html",
+        {
+            "site": site,
+            "news": news,
+            "cat": cat,
+            "subcat": subcat,
+            "lastnews": lastnews,
+            "popnews2": popnews2,
+            "trending": trending,
+        },
+    )
+
+
 def change_pass(request):
 
     # login check start
-    if not request.user.is_authenticated :
-        return redirect('mylogin')
+    if not request.user.is_authenticated:
+        return redirect("mylogin")
     # login check end
 
-    if request.method == 'POST' :
+    if request.method == "POST":
 
-        oldpass = request.POST.get('oldpass')
-        newpass = request.POST.get('newpass')
+        oldpass = request.POST.get("oldpass")
+        newpass = request.POST.get("newpass")
 
-        if oldpass == "" or newpass == "" :
+        if oldpass == "" or newpass == "":
             error = "All Fields Requirded"
-            return render(request, 'back/error.html' , {'error':error})
+            return render(request, "back/error.html", {"error": error})
 
         user = authenticate(username=request.user, password=oldpass)
 
-        if user != None :
+        if user != None:
 
-            if len(newpass) < 8 :
+            if len(newpass) < 8:
                 error = "Your Password Most Be At Less 8 Character"
-                return render(request, 'back/error.html' , {'error':error})
+                return render(request, "back/error.html", {"error": error})
 
             count1 = 0
             count2 = 0
-            count3 = 0 
+            count3 = 0
             count4 = 0
 
-            for i in newpass :
+            for i in newpass:
 
-                if i > "0" and i < "9" :
+                if i > "0" and i < "9":
                     count1 = 1
-                if i > "A" and i < "Z" :
+                if i > "A" and i < "Z":
                     count2 = 1
-                if i > 'a' and i < 'z' :
+                if i > "a" and i < "z":
                     count3 = 1
-                if i > "!" and i < "(" :
+                if i > "!" and i < "(":
                     count4 = 1
 
-            if count1 == 1 and count2 == 1 and count3 == 1 and count4 == 1 :
+            if count1 == 1 and count2 == 1 and count3 == 1 and count4 == 1:
 
                 user = User.objects.get(username=request.user)
                 user.set_password(newpass)
                 user.save()
-                return redirect('mylogout')
+                return redirect("mylogout")
 
         else:
 
             error = "Your Password Is Not Correct"
-            return render(request, 'back/error.html' , {'error':error})
+            return render(request, "back/error.html", {"error": error})
 
-
-    return render(request, 'back/changepass.html')
+    return render(request, "back/changepass.html")
